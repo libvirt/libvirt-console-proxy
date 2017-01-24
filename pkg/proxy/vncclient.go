112f461529e28da7a15d776c41fddda264b9efa3
Daniel P. Berrange <berrange@redhat.com>
2017-01-24 17:22:50 +0000

    Fix proxying of > 64kb of data
    The code was mistakenly shrinking the data buffer on each
    i/o op, until it got to 0 bytes whereupon we'd think we
    had EOF from Read()
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- pkg/proxy/vncclient.go
+++ pkg/proxy/vncclient.go
@@ -295,6 +295,7 @@ func (c *ConsoleClientVNC) Close() {
 func (c *ConsoleClientVNC) proxyData(src net.Conn, dst net.Conn) error {
 	data := make([]byte, 64*1024)
 	pending := 0
+	offset := 0
 	for {
 		if pending == 0 {
 			var err error
@@ -305,14 +306,15 @@ func (c *ConsoleClientVNC) proxyData(src net.Conn, dst net.Conn) error {
 			if pending == 0 {
 				return nil
 			}
+			offset = 0
 		}
 
-		done, err := dst.Write(data[0:pending])
+		done, err := dst.Write(data[offset:pending])
 		if err != nil {
 			return err
 		}
-		data = data[done:]
 		pending -= done
+		offset += done
 	}
 }
 
