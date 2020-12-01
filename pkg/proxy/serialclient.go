eeda6155cde773f2d8bd63b685ee7450a3153c70
Amartey Pearson <apearson@us.ibm.com>
2020-12-01 13:25:52 -0600

    Buffer serial console reads to reduce CPU consumption
    Serial consoles - especially in TLS mode - tend to result in
    lots of small (single digit) byte reads even when pushing large
    amounts of data (cat'ing a large file for example).  The overhead
    of proxying each packet (especially with TLS) can become very CPU
    intensive.
    
    This will buffer reads with a progressively incrementing timeout
    before writing while ensuring the buffer does flush once the timeout
    is hit.  After reading nothing, the timeout will reset thereby
    providing a good interactive user experience.  That is, only when
    sending large volumes of data will the user start to experience
    the choppy buffering.
    
    Signed-off-by: Amartey Pearson <apearson@us.ibm.com>

--- pkg/proxy/serialclient.go
+++ pkg/proxy/serialclient.go
@@ -26,8 +26,10 @@
 package proxy
 
 import (
+	"github.com/golang/glog"
 	"golang.org/x/net/websocket"
 	"net"
+	"time"
 )
 
 type ConsoleClientSerial struct {
@@ -48,28 +50,75 @@ func NewConsoleClientSerial(tenant *websocket.Conn, compute net.Conn) *ConsoleCl
 }
 
 func (c *ConsoleClientSerial) proxyData(src net.Conn, dst net.Conn) error {
-	data := make([]byte, 64*1024)
+	// Name is for logging...
+	name := "T:"
+	if src == c.Compute {
+		name = "C:"
+	}
+
+	const maxDeadlineInMs = 500
+	const deadlineIncrement = 10
+	const bufferSize = 64 * 1024
+	data := make([]byte, bufferSize)
 	pending := 0
 	offset := 0
+	nextDeadline := 0
 	for {
 		if pending == 0 {
+			// In order to reduce CPU consumption, we will continue reading with
+			// a progressively incrementing timeout before writing.  We start with
+			// a blocking read, then increment a deadline of 10ms at a time. Once
+			// we don't read anything within the deadline, we'll write it out.
+			//
+			// This effectively throttles a console from overwhelming the CPU with
+			// thousands of small byte packets (especially over TLS) while providing
+			// a good interactive user experience.
 			var err error
-			pending, err = src.Read(data)
-			if err != nil {
-				return err
+			var deadline time.Time
+			blockingRead := true
+			if nextDeadline == 0 {
+				deadline = time.Time{}
+			} else {
+				deadline = time.Now().Add(time.Duration(nextDeadline) * time.Millisecond)
+				blockingRead = false
 			}
-			if pending == 0 {
-				return nil
+			src.SetReadDeadline(deadline)
+			if nextDeadline <= maxDeadlineInMs {
+				nextDeadline += deadlineIncrement
+			}
+			for err == nil && pending < bufferSize {
+				read, err := src.Read(data[pending:])
+				// If we hit the timeout/deadline...
+				if e, ok := err.(net.Error); ok && e.Timeout() {
+					if read == 0 && pending == 0 {
+						// If we haven't read anything w/in the deadline,
+						// resort to a standard blocking read
+						nextDeadline = 0
+					}
+					pending += read
+					// Break out and write whatever we have (it may be nothing)
+					break
+				} else if err != nil {
+					return err
+				} else if read == 0 {
+					return nil
+				}
+				pending += read
+				if blockingRead {
+					break
+				}
 			}
 			offset = 0
 		}
-
-		done, err := dst.Write(data[offset:pending])
-		if err != nil {
-			return err
+		if pending > 0 {
+			done, err := dst.Write(data[offset:pending])
+			if err != nil {
+				return err
+			}
+			pending -= done
+			offset += done
+			glog.V(3).Infof("%s Wrote %d - deadline %d", name, done, nextDeadline-deadlineIncrement)
 		}
-		pending -= done
-		offset += done
 	}
 }
 
